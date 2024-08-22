319d204a342e9e1a4cf28bec48829f35df8fc310
Ezequiel <32722140+soyezequiel@users.noreply.github.com>
2024-08-22 00:25:16 -0300

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,21 @@
+{
+  "headers": [
+    {
+      "source": "/api/(.*)",
+      "headers": [
+        {
+          "key": "Access-Control-Allow-Origin",
+          "value": "https://walletnaranja.vercel.app"
+        },
+        {
+          "key": "Access-Control-Allow-Methods",
+          "value": "GET, POST, PUT, DELETE, OPTIONS"
+        },
+        {
+          "key": "Access-Control-Allow-Headers",
+          "value": "X-Requested-With, Content-Type, Authorization"
+        }
+      ]
+    }
+  ]
+}
